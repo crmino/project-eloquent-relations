1cb87676408466a0759904a7387ad9ec983b7b6b
Cristóbal Miño <cristobal.rj.cl@gmail.com>
2021-04-14 11:51:48 -0400

    Seeder de Post y Videos (Category, Tags & Comments)

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -30,24 +30,61 @@ class DatabaseSeeder extends Seeder
             $profile->location()->save(factory(App\Location::class)->make());
 
             //pasamos con attach un numero random para que se registre 1,2 o 3 grupos y no uno solo
-            $user->groups()->attach($this->array(rand(1,3)));
+            $user->groups()->attach($this->array(rand(1, 3)));
 
             //imagen creada con un valor por defecto, definido en factory
             //aca ponemos una imagen de perfil con otro valor: 90X90
             $user->image()->save(factory(App\Image::class)->make([
-                'url'=>'https://lorempixel.com/90/90/'
+                'url' => 'https://lorempixel.com/90/90/'
             ]));
 
 
+            //crea 4 categoriras y tags para post y videos
+            factory(App\Category::class, 4)->create();
+            factory(App\Tag::class, 12)->create();
 
+            //crea 40 posts
+            factory(App\Post::class, 40)->create()->each(function ($post) {
+                //crea una imagen al crear el post
+                //1024 x 1024 del factory image
+                $post->image()->save(factory(App\Image::class)->make());
+
+                //usamos el metodo array y pasamos rand 1 al maximo de tag creado (12)
+                $post->tags()->attach($this->array(rand(1, 12)));
+
+                //comentarios
+                $number_comments = rand(1, 6);
+                for ($i = 0; $i < $number_comments; $i++) {
+                    #salvamos cada comentario que se genere
+                    $post->comments()->save(factory(App\Comment::class)->make());
+                }
+            });
+
+            //crea Videos
+            factory(App\Video::class, 40)->create()->each(function ($video) {
+                //crea una imagen al crear el video
+                //1024 x 1024 del factory image
+                $video->image()->save(factory(App\Image::class)->make());
+
+                //usamos el metodo array y pasamos rand 1 al maximo de tag creado (12)
+                $video->tags()->attach($this->array(rand(1, 12)));
+
+                //comentarios
+                $number_comments = rand(1, 6);
+                for ($i = 0; $i < $number_comments; $i++) {
+                    #salvamos cada comentario que se genere
+                    $video->comments()->save(factory(App\Comment::class)->make());
+                }
+            });
         });
     }
 
-    public function array($max){
+    public function array($max)
+    {
         //recibimos max 3 valores, 1,2 o 3
-        $values=[];
-        for ($i=1; $i < $max; $i++) {
-            $values[]=$i;
+        $values = [];
+        for ($i = 1; $i < $max; $i++) {
+            $values[] = $i;
         }
         return $values;
     }
