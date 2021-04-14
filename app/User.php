58b74177a95b925c7e6d2e62be2b66bbdf3ca021
Cristóbal Miño <cristobal.rj.cl@gmail.com>
2021-04-14 13:14:37 -0400

    solucionado error Tags en migracion string 'name'

--- app/User.php
+++ app/User.php
@@ -53,7 +53,7 @@ class User extends Authenticatable
     public function groups()
     {
         //belongsToMany = pertence y tiene muchos
-        return $this->belongsToMany(Group::class);
+        return $this->belongsToMany(Group::class)->withTimestamps();
     }
 
     //un user tiene una location a traves de profile
